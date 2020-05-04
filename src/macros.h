9b0d52e92403bbbbf4c9b6bd7aab8e2636932141
Gabor Heja <gheja@m00box.git.stdio.hu>
2020-05-04 18:41:15 +0200

    Updated bit manipulation macros

--- src/macros.h
+++ src/macros.h
@@ -1,5 +1,6 @@
-#define BIT_SET(a, b) a |= _BV(b)
-#define BIT_CLEAR(a, b) a &= ~_BV(b)
+#define BIT_GET(a, b) (a & _BV(b))
+#define BIT_SET(a, b) (a |= _BV(b))
+#define BIT_CLEAR(a, b) (a &= ~_BV(b))
 
 #define RADIO_ON BIT_SET(PORT, PIN_RADIO)
 #define RADIO_OFF BIT_CLEAR(PORT, PIN_RADIO)
